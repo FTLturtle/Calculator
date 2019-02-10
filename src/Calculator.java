696d818c1dd45abdd2df92e891ba8e7861912c7e
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-10 13:14:24 -0500

    finished calc method, added private getter and setter methods, added memory methods, added ability to change trig units

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,52 +1,179 @@
+/**
+ * This program makes use of the mXparser package developed by Mariusz Gromada:
+ * http://mathparser.org/api/org/mariuszgromada/math/mxparser/package-summary.html
+ *
+ * To get the code to run, you need to add the library MathParser.org-mXparser-v.4.2.0-jdk.1.8.jar
+ *
+ * That library is included in the folder "lib"
+ */
+
 import org.mariuszgromada.math.mxparser.*;
 
 public class Calculator {
-    private String displayMode;
-    private String unitMode;
+    private int base;
+    private String trigUnits;
     private double memory;
+    private double currentValue;
 
     public Calculator(){
-        displayMode = "decimal";
-        unitMode = "radian";
+        base = 10;
+        trigUnits = "radians";
         memory = 0;
+        currentValue = 0;
     }
 
+    // PUBLIC METHODS:
+    /**
+     * This method takes a String as input (e.g., "1 + 2 * 3"), interprets the string as a mathematical
+     * expression, calculates the value of the expression, and returns the value as a String (e.g., "7").
+     */
     public String calc(String input){
+
+        // Checking for degrees or radians mode
+        if (trigUnits.equals("degrees")) {
+            mXparser.setDegreesMode();
+        } else {
+            mXparser.setRadiansMode();
+        }
+
         Expression expression = new Expression(input);
-        double value = expression.calculate();
-        String result = "" + value;
+        this.currentValue = expression.calculate();
+
+        String result = "" + this.currentValue;
         return result;
     }
 
-    public double getMemory() {
+    /**
+     *
+     * @return
+     */
+    public String getCurrentValueAsString(){
+        return "" + this.currentValue;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getMemoryAsString(){
+        return "" + this.memory;
+    }
+
+    /**
+     *
+     */
+    public void addCurrentValueToMemory(){
+        this.memory = this.memory + this.currentValue;
+    }
+
+    /**
+     *
+     */
+    public void resetMemory(){
+        this.memory = 0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getBase() {
+        return base;
+    }
+
+    /**
+     *
+     * @param newBase
+     */
+    public void setBase(int newBase) {
+        if (newBase >= 1 && newBase <= 36) {
+            this.base = newBase;
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getTrigUnits() {
+        return trigUnits;
+    }
+
+    /**
+     *
+     * @param newTrigUnits
+     */
+    public void setTrigUnits(String newTrigUnits) {
+        if (newTrigUnits.equals("degrees") || newTrigUnits.equals("radians")) {
+            this.trigUnits = newTrigUnits;
+        }
+    }
+
+
+
+    // PRIVATE METHODS:
+    /**
+     *
+     * @return
+     */
+    private double getCurrentValue() {
+        return currentValue;
+    }
+
+    /**
+     *
+     * @param currentValue
+     */
+    private void setCurrentValue(double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    /**
+     *
+     * @return
+     */
+    private double getMemory() {
         return memory;
     }
 
-    public void setMemory(double memory) {
+    /**
+     *
+     * @param memory
+     */
+    private void setMemory(double memory) {
         this.memory = memory;
     }
 
-    public void addToCurrentMemory(double addToMemory){
+    /**
+     *
+     * @param addToMemory
+     */
+    private void addToCurrentMemory(double addToMemory){
         this.memory = this.memory + addToMemory;
     }
 
-    public String getDisplayMode() {
-        return displayMode;
+
+    /**
+     *
+     * @param expressionToConvert
+     * @param originalBase
+     * @return
+     */
+    private static String convertExpressionToDecimal(String expressionToConvert, int originalBase){
+        if (originalBase == 10){
+            return expressionToConvert;
+        }
+
+
+
+        return "";
     }
 
-    public void setDisplayMode(String displayMode) {
-        this.displayMode = displayMode;
+    private String convertCurrentValueToOtherBase(){
+        double resultToConvert = this.currentValue;
+        String convertedResult = mXparser.convDecimal2OthBase(resultToConvert, this.base);
+
+        return convertedResult;
     }
 
-    public String getUnitMode() {
-        return unitMode;
-    }
-
-    public void setUnitMode(String unitMode) {
-        this.unitMode = unitMode;
-    }
-
-    private void updateLastValue(){
-
-    }
 }
