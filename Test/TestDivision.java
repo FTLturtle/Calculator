bb371e4021f13436be9a242fa4a9dd2b4c58e8af
Brian Wong <brianwong@zipcoders-MacBook-Pro.local>
2019-02-10 16:35:31 -0500

    Test for +,-,*,/

---
+++ Test/TestDivision.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestDivision {
+    public Calculator calc = new Calculator();
+
+    @Test
+    public void testDivision1() {
+
+        //Input,Given,Threshold
+        String input = "4/2";
+        Double expected =  2.0;
+        Double threshold = .0001;
+
+        //When
+        Double actual = Double.parseDouble(calc.calc(input));
+
+        //Then
+        assertEquals(expected, actual, threshold);
+
+    }
+
+    @Test
+    public void TestDivision2() {
+
+        //Input,Given,Threshold
+        String input ="10/3";
+        Double expected = 3.3333;
+        Double threshold = .0001;
+
+        //When
+        Double actual = Double.parseDouble(calc.calc(input));
+
+        //Then
+        assertEquals(expected, actual, threshold);
+    }
+
+    @Test
+    public void TestDivision3() {
+
+        //Input,Given,Threshold
+        String input ="5/2";
+        Double expected = 2.5;
+        Double threshold = .0001;
+
+        //When
+        Double actual = Double.parseDouble(calc.calc(input));
+
+        //Then
+        assertEquals(expected, actual, threshold);
+
+    }
+
+    @Test
+    public void TestDivision4() {
+
+        //Input,Given,Threshold
+        String input ="0/0";
+        String expected = "NaN";
+
+        //When
+        String actual = calc.calc(input);
+
+        //Then
+        assertEquals(expected, actual);
+
+    }
+}
---
+++ Test/TestSubtraction.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestSubtraction {
+    public  Calculator calc = new Calculator();
+
+    @Test
+    public void testSubtraction1() {
+
+        //Input
+        String input ="49-6";
+        String expected = "43.0";
+
+        //When
+        String actual = calc.calc(input);
+
+        //Then
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testSubtraction2() {
+
+        //Input
+        String input ="951-111";
+        String expected = "840.0";
+
+        //When
+        String actual = calc.calc(input);
+
+        //Then
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testSubtraction3() {
+
+        //Input
+        String input ="-950-50";
+        String expected = "-1000.0";
+
+        //When
+        String actual = calc.calc(input);
+
+        //Then
+        assertEquals(expected,actual);
+
+    }
+}
