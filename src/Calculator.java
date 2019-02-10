d980c8108648dec6aa98077896687bce627bd087
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-09 20:14:16 -0500

    added functionality to calculator class: ability  to evaluate any expression in decimal

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,48 @@
-import org.mariuszgromada.math.mxparser.Expression;
+import org.mariuszgromada.math.mxparser.*;
 
 public class Calculator {
+    private String displayMode;
+    private String unitMode;
+    private double memory;
 
-    public static void calc(){
-        Expression x = new Expression("1 / 6");
-        System.out.println(x.calculate());
+    public Calculator(){
+        displayMode = "decimal";
+        unitMode = "radian";
+        memory = 0;
+    }
+
+    public String calc(String input){
+        Expression expression = new Expression(input);
+        double value = expression.calculate();
+        String result = "" + value;
+        return result;
+    }
+
+    public double getMemory() {
+        return memory;
+    }
+
+    public void setMemory(double memory) {
+        this.memory = memory;
+    }
+
+    public String getDisplayMode() {
+        return displayMode;
+    }
+
+    public void setDisplayMode(String displayMode) {
+        this.displayMode = displayMode;
+    }
+
+    public String getUnitMode() {
+        return unitMode;
+    }
+
+    public void setUnitMode(String unitMode) {
+        this.unitMode = unitMode;
+    }
+
+    private void updateLastValue(){
+
     }
 }
